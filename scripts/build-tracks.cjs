83ab486171c178a6571b7bfb21b94d575ad41c05
divyprj <surajdivyansh104@gmail.com>
2026-08-25 17:07:59 +0530

    feat: add all 29 full Lunch Break studio tracks

---
+++ scripts/build-tracks.cjs
@@ -0,0 +1,39 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const audioDir = path.join(__dirname, '..', 'public', 'audio');
+const files = fs.readdirSync(audioDir).filter(f => f.endsWith('.m4a') || f.endsWith('.mp3') || f.endsWith('.opus'));
+
+console.log('Found audio files:', files.length);
+
+const cleanTitle = (filename) => {
+  return filename
+    .replace(/\s*\[.*?\]\.\w+$/, '')
+    .replace(/\.\w+$/, '')
+    .trim();
+};
+
+const tracks = files.map((file, i) => {
+  const title = cleanTitle(file);
+  let artist = 'Seedhe Maut';
+  if (title.toLowerCase().includes('khatta flow')) artist = 'Seedhe Maut x KR$NA';
+  else if (title.toLowerCase().includes('brand new')) artist = 'Seedhe Maut ft. Faris Shafi';
+  else if (title.toLowerCase().includes('swah')) artist = 'Seedhe Maut ft. Badshah';
+  else if (title.toLowerCase().includes('asal g')) artist = 'Seedhe Maut ft. Farhan Khan';
+  
+  return {
+    id: 'track-' + (i + 1),
+    title: title,
+    artist: artist,
+    cover: 'vinylCover',
+    audioUrl: '/audio/' + encodeURIComponent(file)
+  };
+});
+
+const content = `import vinylCover from './assets/vinyl-cover.jpg';
+
+export const TRACKS = ${JSON.stringify(tracks, null, 2).replace(/"cover": "vinylCover"/g, '"cover": vinylCover')};
+`;
+
+fs.writeFileSync(path.join(__dirname, '..', 'src', 'tracks.js'), content, 'utf8');
+console.log('src/tracks.js updated successfully with all ' + tracks.length + ' tracks!');
--- src/tracks.js
+++ src/tracks.js
@@ -1,39 +1,207 @@
-﻿import vinylCover from './assets/vinyl-cover.jpg';
+import vinylCover from './assets/vinyl-cover.jpg';
 
 export const TRACKS = [
   {
-    id: 'khatta-flow',
-    title: 'Khatta Flow',
-    artist: 'Seedhe Maut x KR$NA',
-    cover: vinylCover,
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3?filename=trap-future-bass-royalty-free-music-111037.mp3',
+    "id": "track-1",
+    "title": "11K",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/11K%20%5Bl79W7TCggyk%5D.m4a"
   },
   {
-    id: '11k',
-    title: '11K',
-    artist: 'Seedhe Maut',
-    cover: vinylCover,
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2022/01/18/audio_d0a13f69d2.mp3?filename=hip-hop-rock-beat-10904.mp3',
+    "id": "track-2",
+    "title": "Akatsuki",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Akatsuki%20%5B5Jmhm3LoKao%5D.m4a"
   },
   {
-    id: 'luka-chippi',
-    title: 'Luka Chippi',
-    artist: 'Seedhe Maut',
-    cover: vinylCover,
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2022/10/14/audio_9939f792cb.mp3?filename=drill-beat-123497.mp3',
+    "id": "track-3",
+    "title": "Asal G",
+    "artist": "Seedhe Maut ft. Farhan Khan",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Asal%20G%20%5BimaDr8QausY%5D.m4a"
   },
   {
-    id: 'brand-new',
-    title: 'Brand New',
-    artist: 'Seedhe Maut ft. Faris Shafi',
-    cover: vinylCover,
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2021/08/04/audio_12b0c7443c.mp3?filename=boom-bap-hip-hop-10705.mp3',
+    "id": "track-4",
+    "title": "Baat Aisi Ghar Jaisi",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Baat%20Aisi%20Ghar%20Jaisi%20%5BXiz0CW5Xpx4%5D.m4a"
   },
   {
-    id: 'champions',
-    title: 'Champions',
-    artist: 'Seedhe Maut',
-    cover: vinylCover,
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2023/04/18/audio_6cf0c2d3cf.mp3?filename=action-hip-hop-146313.mp3',
+    "id": "track-5",
+    "title": "Brand New",
+    "artist": "Seedhe Maut ft. Faris Shafi",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Brand%20New%20%5BRFEVbcFgJdU%5D.m4a"
+  },
+  {
+    "id": "track-6",
+    "title": "Champions",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Champions%20%5BWGlu7qJCOIk%5D.m4a"
+  },
+  {
+    "id": "track-7",
+    "title": "Dikkat",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Dikkat%20%5BB9nKEZeAq4I%5D.m4a"
+  },
+  {
+    "id": "track-8",
+    "title": "Fanne Khan",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Fanne%20Khan%20%5B25ruuvtj31g%5D.m4a"
+  },
+  {
+    "id": "track-9",
+    "title": "First Place",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/First%20Place%20%5B3Ex7QYgXkuI%5D.m4a"
+  },
+  {
+    "id": "track-10",
+    "title": "Focused Sedated",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Focused%20Sedated%20%5BM54URfWchD8%5D.m4a"
+  },
+  {
+    "id": "track-11",
+    "title": "Hausla",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Hausla%20%5BRVF2CRMXKJQ%5D.m4a"
+  },
+  {
+    "id": "track-12",
+    "title": "I Don't Miss That Life",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/I%20Don't%20Miss%20That%20Life%20%5BtVljR5oTKrc%5D.m4a"
+  },
+  {
+    "id": "track-13",
+    "title": "Joint in the Booth",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Joint%20in%20the%20Booth%20%5BB4ex_KCiVpE%5D.m4a"
+  },
+  {
+    "id": "track-14",
+    "title": "Kehna Chahte Hain...",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Kehna%20Chahte%20Hain...%20%5BflZ_LohtqB4%5D.m4a"
+  },
+  {
+    "id": "track-15",
+    "title": "Khatta Flow",
+    "artist": "Seedhe Maut x KR$NA",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Khatta%20Flow%20%5BIBkT4Yww7zk%5D.m4a"
+  },
+  {
+    "id": "track-16",
+    "title": "Khauf",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Khauf%20%5BzY2xL4Zvhf4%5D.m4a"
+  },
+  {
+    "id": "track-17",
+    "title": "Khoon",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Khoon%20%5BhInbR9Qm8cQ%5D.m4a"
+  },
+  {
+    "id": "track-18",
+    "title": "Kya Challa",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Kya%20Challa%20%5BIExPsNSBeu8%5D.m4a"
+  },
+  {
+    "id": "track-19",
+    "title": "Luka Chippi",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Luka%20Chippi%20%5B9mH-57TvCGo%5D.m4a"
+  },
+  {
+    "id": "track-20",
+    "title": "Lunch Break",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Lunch%20Break%20%5Bdj0O_6NX59Y%5D.m4a"
+  },
+  {
+    "id": "track-21",
+    "title": "Naam Kaam Sheher",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Naam%20Kaam%20Sheher%20%5BVa2efwPb8Ao%5D.m4a"
+  },
+  {
+    "id": "track-22",
+    "title": "Off Beat",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Off%20Beat%20%5B6BrBgsYYXUg%5D.m4a"
+  },
+  {
+    "id": "track-23",
+    "title": "Pain",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Pain%20%5BJmYYK3rWnVk%5D.m4a"
+  },
+  {
+    "id": "track-24",
+    "title": "Peace of Mind",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Peace%20of%20Mind%20%5BpaOFscgw7Vg%5D.m4a"
+  },
+  {
+    "id": "track-25",
+    "title": "Pushpak Vimaan",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Pushpak%20Vimaan%20%5BgAkbSydU3kw%5D.m4a"
+  },
+  {
+    "id": "track-26",
+    "title": "Sick & Proper",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Sick%20%26%20Proper%20%5BfOt5gw9dZhY%5D.m4a"
+  },
+  {
+    "id": "track-27",
+    "title": "Swah!",
+    "artist": "Seedhe Maut ft. Badshah",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Swah!%20%5BKy-QenzQD6U%5D.m4a"
+  },
+  {
+    "id": "track-28",
+    "title": "Taakat",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/Taakat%20%5By6ABG8tjfbw%5D.m4a"
+  },
+  {
+    "id": "track-29",
+    "title": "W",
+    "artist": "Seedhe Maut",
+    "cover": vinylCover,
+    "audioUrl": "/audio/W%20%5BcV6sq4ByyK8%5D.m4a"
   }
 ];
